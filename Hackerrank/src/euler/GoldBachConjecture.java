24cb10b68296f590e4ad78c419c48aaaf93992fb
Kartik <rama354@gmail.com>
2016-04-04 13:16:29 +0530

    https://www.hackerrank.com/contests/projecteuler/challenges/euler046

---
+++ Hackerrank/src/euler/GoldBachConjecture.java
@@ -0,0 +1,60 @@
+package euler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GoldBachConjecture {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner sc=new Scanner(System.in);
+		 int T=sc.nextInt();
+		 List<Integer> primes=new ArrayList<Integer>();
+		 while (T>0){
+			 int N=sc.nextInt();
+			 System.out.println(goldBachConjecture(N));	 	 
+			 T--;
+		 }
+    }
+    
+	private static int goldBachConjecture(int n) {
+		 int count=0;
+		 for(int i=2;i<n-1;i++)
+		 { 
+			 if (isPrime(i))
+			 {
+				 int j=n-i;
+				 double sqrt=Math.sqrt(j/2);
+                 if (j%2==0 && Math.ceil(sqrt)==sqrt)
+                	 count++;
+                 
+			 }
+			 
+		 }
+		
+		 return count;
+	}
+   
+    
+    private static boolean isPrime(int i) 
+    {
+    	if (i>2 && i%2==0)
+    		return false;
+    	
+    	int sqrt=(int) Math.sqrt(i);
+    
+    	for (int j=3;j<=sqrt;j+=2)
+    		if (i%j==0)
+    			return false;
+    	
+    	return true;
+	}
+
+
+	
+}
+

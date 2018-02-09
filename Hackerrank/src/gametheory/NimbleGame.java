7c4a3f6904d9c5b4668b874dac159421e4e54222
Ramaswamy R <r79@avaya.com>
2018-02-09 15:10:54 +0530

    https://www.hackerrank.com/challenges/nimble-game-1/problem

--- Hackerrank/src/gametheory/NimbleGame.java
+++ Hackerrank/src/gametheory/NimbleGame.java
@@ -1,10 +1,32 @@
 package gametheory;
 
+import java.util.Scanner;
+
 public class NimbleGame {
 
+	static String nimbleGame(int[] s) {
+        // Variant of Standard Nim Game https://www.math.ucla.edu/~tom/Game_Theory/comb.pdf
+		int nimsum=0;
+		for (int i=1;i<s.length;i++){
+			if (s[i]%2==1){
+				nimsum^=i;
+			}
+		}
+		return nimsum>0?"First":"Second";
+    }
+	
 	public static void main(String[] args) {
-		
-		
-		
-	}
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int[] s = new int[n];
+            for(int s_i = 0; s_i < n; s_i++){
+                s[s_i] = in.nextInt();
+            }
+            String result = nimbleGame(s);
+            System.out.println(result);
+        }
+        in.close();
+    }
 }
